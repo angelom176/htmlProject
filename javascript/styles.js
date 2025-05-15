4c8ab21658443c8b074331c7ad7dc3224d4be245
Angelo Jose <angelojose.medeirosdemacedofilho@student.griffith.ie>
2025-05-15 02:48:30 +0100

    fixing bug in javascript/styles.js

--- javascript/styles.js
+++ javascript/styles.js
@@ -66,30 +66,6 @@ function startCourse() {
 
 
 
-const carrossel = document.getElementById('carrossel');
-const btnAnterior = document.getElementById('btnAnterior');
-const btnProximo = document.getElementById('btnProximo');
-
-const scrollBy = 280;
-
-btnAnterior.addEventListener('click', () => {
-  carrossel.scrollBy({ left: -scrollBy, behavior: 'smooth' });
-});
-
-btnProximo.addEventListener('click', () => {
-  carrossel.scrollBy({ left: scrollBy, behavior: 'smooth' });
-});
-
-const progress = document.getElementById('progress');
-
-// Atualiza a largura da barra de progresso conforme a rola
-carrossel.addEventListener('scroll', () => {
-  const scrollLeft = carrossel.scrollLeft;
-  const scrollWidth = carrossel.scrollWidth - carrossel.clientWidth;
-  const percentage = (scrollLeft / scrollWidth) * 100;
-  progress.style.width = `${percentage}%`;
-});
-
 document.querySelectorAll('.faq-question').forEach(button => {
   button.addEventListener('click', () => {
     const item = button.closest('.faq-item');
